0d7c8c13d3c88b70ebab180fb9d799b168407f6f
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-02-18 19:22:35 -0700

    fit: creación del proyecto

---
+++ Proyecto01_262722_262742/src/main/java/com/mycompany/proyecto01_262722_262742/Proyecto01_262722_262742.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto01_262722_262742;
+
+/**
+ *
+ * @author jesus
+ */
+public class Proyecto01_262722_262742 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
