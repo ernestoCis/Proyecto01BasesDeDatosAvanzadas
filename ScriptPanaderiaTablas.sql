109a6efd3bc3a032e2de098fd8df158d37b73f02
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-02-18 18:43:40 -0700

    script corregido despues de maye

--- ScriptPanaderiaTablas.sql
+++ ScriptPanaderiaTablas.sql
@@ -28,45 +28,15 @@ CREATE TABLE ProductosIngredientes(
     FOREIGN KEY(id_ingrediente) REFERENCES Ingredientes(id)
 );
 
--- Tabla Pedidos
-CREATE TABLE Pedidos(
+-- Tabla Cupones
+CREATE TABLE Cupones(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    estado ENUM("Pendiente", "En preparación", "Listo", "Entregado", "Cancelado", "No reclamado") NOT NULL,
-    fecha DATE NOT NULL
-);
-
--- Tabla PedidosExpress
-CREATE TABLE PedidosExpress(
-	id_pedido INT PRIMARY KEY,
-    pin INT NOT NULL,
-    folio VARCHAR(10) NOT NULL,
-    FOREIGN KEY(id_pedido) REFERENCES Pedidos(id)
-);
-
--- Tabla PedidosProgramados
-CREATE TABLE PedidosProgramados(
-	id_pedido INT PRIMARY KEY AUTO_INCREMENT,
-    numero_pedido INT NOT NULL,
-    FOREIGN KEY(id_pedido) REFERENCES Pedidos(id)
-);
-
--- Tabla DetallesPedidos
-CREATE TABLE DetallesPedidos(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nota VARCHAR(100),
-    cantidad INT NOT NULL,
-    total FLOAT NOT NULL,
-    id_pedido INT NOT NULL,
-	FOREIGN KEY(id_pedido) REFERENCES Pedidos(id)
-);
-
--- Tabla ProductosDetallesPedidos
-CREATE TABLE ProductosDetallesPedidos(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    id_producto INT NOT NULL,
-    FOREIGN KEY(id_producto) REFERENCES Productos(id),
-    id_detalle_pedido INT NOT NULL,
-    FOREIGN KEY(id_detalle_pedido) REFERENCES DetallesPedidos(id)
+    descuento INT NOT NULL,
+    fecha_vencimiento DATE,
+    fecha_inicio DATE NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    numero_usos INT NOT NULL,
+    tope_usos INT NOT NULL
 );
 
 -- Tabla usuarios
@@ -109,25 +79,46 @@ CREATE TABLE Telefonos(
 
 -- Tabla Empleados
 CREATE TABLE Empleados(
-	id_cliente INT NOT NULL,
-    FOREIGN KEY(id_cliente) REFERENCES Clientes(id_usuario)
+	id_usuario INT NOT NULL,
+    FOREIGN KEY(id_usuario) REFERENCES Usuarios(id)
 );
 
-
--- Tabla Cupones
-CREATE TABLE Cupones(
+-- Tabla Pedidos
+CREATE TABLE Pedidos(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    descuento INT NOT NULL,
-    fecha_vencimiento DATE,
-    nombre VARCHAR(30) NOT NULL,
-    numero_usos INT NOT NULL
+    estado ENUM("Pendiente", "En preparación", "Listo", "Entregado", "Cancelado", "No reclamado") NOT NULL,
+    fecha_creacion DATE NOT NULL,
+    fecha_entrega DATE NOT NULL,
+    metodo_pago ENUM("Efectivo", "Credito", "Debito") NOT NULL,
+    numero_pedido INT UNIQUE NOT NULL,
+    id_cliente INT,
+    FOREIGN KEY(id_cliente) REFERENCES Clientes(id_usuario) 
 );
 
--- Tabla PedidosProgramadosCupones
-CREATE TABLE PedidosProgramadosCupones(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    id_pedido INT NOT NULL,
-    FOREIGN KEY(id_pedido) REFERENCES Pedidos(id),
-    id_cupon INT NOT NULL,
+-- Tabla PedidosExpress
+CREATE TABLE PedidosExpress(
+	id_pedido INT PRIMARY KEY,
+    pin INT NOT NULL,
+    folio VARCHAR(10) NOT NULL,
+    FOREIGN KEY(id_pedido) REFERENCES Pedidos(id)
+);
+
+-- Tabla PedidosProgramados
+CREATE TABLE PedidosProgramados(
+	id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    id_cupon INT,
     FOREIGN KEY(id_cupon) REFERENCES Cupones(id)
+);
+
+-- Tabla DetallesPedidos
+CREATE TABLE DetallesPedidos(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nota VARCHAR(100),
+    cantidad INT NOT NULL,
+    precio FLOAT NOT NULL,
+    total FLOAT NOT NULL,
+    id_pedido INT NOT NULL,
+	FOREIGN KEY(id_pedido) REFERENCES Pedidos(id),
+    id_producto INT NOT NULL,
+    FOREIGN KEY(id_producto) REFERENCES Productos(id)
 );
