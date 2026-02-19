d3ea7e22d11021d6cf255b28487097f4762fedef
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-02-19 00:56:12 -0700

    BO pendiente de terminar

---
+++ Proyecto01_262722_262742/src/main/java/negocio/BOs/iProductoBO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package negocio.BOs;
+
+import dominio.Producto;
+import negocio.Excepciones.NegocioException;
+
+/**
+ *
+ * @author jesus
+ */
+public interface iProductoBO {
+    /**
+     * Metodo para consultar un producto a la BD
+     * @param producto producto a buscar
+     * @return regresa el producto consultado
+     * @throws NegocioException excepcion por regla de negocio
+     */
+    public Producto consultarProducto(Producto producto) throws NegocioException;
+    
+    /**
+     * metodo para agregar un preoducto a la BD mediante DTO
+     * @param producto producto a ingresar
+     * @return producto insertado
+     * @throws NegocioException NegocioException excepcion por regla de negocio
+     */
+    public Producto insertarProducto(Producto producto) throws NegocioException;
+    
+    /**
+     * metodo para actualizar a un produtco en la BD
+     * @param producto producto a actualizazar
+     * @return producto actualizado
+     * @throws NegocioException NegocioException excepcion por regla de negocio
+     */
+    public Producto actualizarProducto(Producto producto) throws NegocioException;
+}
