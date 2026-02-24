48889e3007ebf15092912e8dbd97ee4f7c67ea91
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-02-24 02:19:47 -0700

    cambio de PIN int a String

--- Proyecto01_262722_262742/src/main/java/dominio/PedidoExpress.java
+++ Proyecto01_262722_262742/src/main/java/dominio/PedidoExpress.java
@@ -12,23 +12,23 @@ import java.time.LocalDateTime;
  */
 public class PedidoExpress extends Pedido {
 
-    private int pin;
+    private String pin;
     private String folio;
 
     public PedidoExpress() {
     }
 
-    public PedidoExpress(int id, EstadoPedido estado, LocalDateTime fechaCreacion, LocalDateTime fechaEntrega, MetodoPago metodoPago, float total, int numeroPedido, Cliente cliente, int pin, String folio) {
+    public PedidoExpress(int id, EstadoPedido estado, LocalDateTime fechaCreacion, LocalDateTime fechaEntrega, MetodoPago metodoPago, float total, int numeroPedido, Cliente cliente, String pin, String folio) {
         super(id, estado, fechaCreacion, fechaEntrega, metodoPago, total, numeroPedido, cliente);
         this.pin = pin;
         this.folio = folio;
     }
 
-    public int getPin() {
+    public String getPin() {
         return pin;
     }
 
-    public void setPin(int pin) {
+    public void setPin(String pin) {
         this.pin = pin;
     }
 
